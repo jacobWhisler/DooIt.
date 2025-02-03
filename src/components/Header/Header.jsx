09d27b4427100e5a80003dec8879349e5b8a945e
Jacob Whisler <jacob.whisler0@gmail.com>
2025-02-02 22:25:15 -0500

    file clean up and header created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h2>DooIt.</h2>
+      <p>Task Tracker</p>
+    </div>
+  );
+};
+
+export default Header;
